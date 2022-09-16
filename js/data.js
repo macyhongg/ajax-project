5d0bde3f9aecc82a02716f2f26f92932de03cfc4
Macy Hong <macyhong@gmail.com>
2022-09-16 05:34:14 +0000

    Create HTML skeleton for Details page #2

--- js/data.js
+++ js/data.js
@@ -7,6 +7,7 @@ var xhr = new XMLHttpRequest();
 xhr.open('GET', 'https://ghibliapi.herokuapp.com/films');
 xhr.responseType = 'json';
 
+// Browse Page
 xhr.addEventListener('load', function () {
   // console.log('xhr status:', xhr.status);
   // console.log('xhr response:', xhr.response);
@@ -27,4 +28,36 @@ xhr.addEventListener('load', function () {
     $cards.appendChild($card);
   }
 });
+
+// Details Page
+
+xhr.addEventListener('click', function () {
+  // console.log('xhr status:', xhr.status);
+  // console.log('xhr response:', xhr.response);
+
+  document.addEventListener('click', function (event) {
+    // console.log('click!');
+    if (event.target.matches('h2') || event.target.matches('.card')) {
+      // console.log('title has been clicked!');
+    }
+  });
+
+  for (let i = 0; i < xhr.response.length; i++) {
+    var $card = document.createElement('section');
+    var $h2 = document.createElement('h2');
+    $h2.textContent = xhr.response[i].title;
+    var $img = document.createElement('img');
+    $img.setAttribute('src', xhr.response[i].image);
+    $img.className = 'card';
+    var $p = document.createElement('p');
+    $p.textContent = xhr.response[i].description;
+
+    $card.appendChild($h2);
+    $card.appendChild($img);
+    $card.appendChild($p);
+
+    $cards.appendChild($card);
+  }
+});
+
 xhr.send();
---
+++ js/main.js
@@ -0,0 +1,2 @@
+// var $title = document.querySelectorAll('h2');
+// var $filmCard = document.querySelectorAll('.card');
